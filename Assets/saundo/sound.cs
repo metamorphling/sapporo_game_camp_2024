5435a37fd3d3100a6ef718ca07a9176e31d35f3f
tutch-k <2223053@stu.hus.ac.jp>
2024-10-12 18:42:22 +0900

    音源

--- Assets/saundo/sound.cs
+++ Assets/saundo/sound.cs
@@ -10,38 +10,55 @@ public class sound : MonoBehaviour
     [SerializeField] private AudioSource drill;
     [SerializeField] private AudioSource err;
     [SerializeField] private AudioSource break1;
+    [SerializeField] private AudioSource attak;
+    [SerializeField] private AudioSource kria;
+    [SerializeField] private AudioSource over;
 
     [SerializeField] private AudioClip click1sound;
     [SerializeField] private AudioClip jampsund;
     [SerializeField] private AudioClip drillsound;
     [SerializeField] private AudioClip errsound;
     [SerializeField] private AudioClip break1sound;
+    [SerializeField] private AudioClip attaksound;
+    [SerializeField] private AudioClip kriasound;
+    [SerializeField] private AudioClip oversound;
 
-
-
-    public void click()
+    public void click()//ボタンクリック
     {
         click1.PlayOneShot(click1sound);
     }
 
-    public void jamp1()
+    public void jamp1()//ジャンプ
     {
         jamp.PlayOneShot(jampsund);
     }
 
-    public void drill1()
+    public void drill1()//攻撃（接触）
     {
         drill.PlayOneShot(drillsound);
     }
 
-    public void err1()
+    public void err1()//壊せない
     {
         err.PlayOneShot(errsound);
     }
 
-    public void brek()
+    public void brek()//ブロック破壊
     {
         break1.PlayOneShot(break1sound);
     }
+    public void attakku1()//岩石一回目
+    {
+        attak.PlayOneShot(attaksound);
+    }
 
+    public void kria1()//ゲームクリア
+    {
+        kria.PlayOneShot(kriasound);
+    }
+
+    public void over1()//ゲームオーバー
+    {
+        over.PlayOneShot(oversound);
+    }
 }
