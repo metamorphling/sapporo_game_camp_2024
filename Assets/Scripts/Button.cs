ba5b13496f8e359c388a60bdaf0e1f7ecd2035df
tetsuji yamamoto <241325@st.yoshida-g.ac.jp>
2024-10-13 13:34:31 +0900

    ポーズ作成中

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    //[SerializeField] Button button;
+
+    public Canvas canvas;            // Canvasをアサイン
+    public GameObject[] uiElements; // ButtonUIを格納する配列
+    private bool isVisible = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        // 初期状態ではすべてのUI要素を非表示にする
+        foreach (GameObject uiElement in uiElements)
+        {
+            uiElement.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // タブキーが押されたかをチェック
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            // 表示状態を切り替える
+            isVisible = !isVisible;
+
+            // Canvasが非アクティブならアクティブにする
+            if (!canvas.gameObject.activeSelf)
+            {
+                canvas.gameObject.SetActive(true);
+            }
+
+            // 表示状態に応じてすべてのUI要素の表示を切り替える
+            foreach (GameObject uiElement in uiElements)
+            {
+                uiElement.SetActive(isVisible);
+            }
+
+          
+        }
+    }
+
+    // ボタンが押された場合、今回呼び出される関数
+    public void ClickContenue()
+    {
+        Debug.Log("押された1!");  // ログを出力
+    }
+
+    // ボタンが押された場合、今回呼び出される関数
+    public void ClickRetry()
+    {
+        Debug.Log("押された2!");  // ログを出力
+    }
+
+    // ボタンが押された場合、今回呼び出される関数
+    public void ClickQuit()
+    {
+        Debug.Log("押された3!");  // ログを出力
+    }
+}
